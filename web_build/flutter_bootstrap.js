ad44dc91587af3ba5e9025413be064aad9d6801c
RitikM-AiDev <ritik.aidev@gmail.com>
2026-09-09 10:04:44 +0530

    fix(web): fix timetable period slot selection blank page and API routing

--- web_build/flutter_bootstrap.js
+++ web_build/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"c416acfeb8126e097f758c664aaa3da929e27d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "562091664" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3958710229" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
